8619a21ef3e39feae3b9f7e4e5a332c789eac6c8
nanko89 <natali.v.angelova@gmail.com>
2022-04-14 18:07:48 +0300

    Fundamentals - Part 1

---
+++ Fundamentals - Part 1/hello.js
@@ -0,0 +1,25 @@
+//LECTURE: Values and Variables
+
+const country = "Bulgaria";
+const continent = "Europe";
+let population = 7.5;
+
+console.log(country, continent, population);
+
+//LECTURE: Data Types
+
+const isIsland = false;
+const language = "Bulgarian";
+
+console.log(isIsland + " " + population + " " + country + " " + language)
+
+//LECTURE: let, const and var
+
+/*const country = "Bulgaria";
+const continent = "Europe";
+const isIsland = false;
+const language = "Bulgarian";*/
+
+
+//LECTURE: Basic Operators
+
