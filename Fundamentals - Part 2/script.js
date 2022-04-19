5b7566d8c264c2c056a6ae69f1562cb8964d3e5e
Натали Ангелова <86059352+nanko89@users.noreply.github.com>
2022-04-19 23:30:11 +0300

    Update script.js

--- Fundamentals - Part 2/script.js
+++ Fundamentals - Part 2/script.js
@@ -74,6 +74,37 @@ console.log(describePopulation('Bulgaria', 6.9));
 console.log(describePopulation('France', 67.4));
 console.log(describePopulation('England', 56));
 
+//-------------------------------------------------------------------------------
+//Coding Challenge #1
+
+const calcAverage = (first, second, third) => (first + second + third) / 3;
+
+// Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+
+const avgDolhins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+
+// Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+// const avgDolhins = calcAverage(85, 54, 41);
+// const avgKoalas = calcAverage(23, 34, 27);
+
+console.log(avgDolhins, avgKoalas);
+
+function checkWinner(dolhins, koalas) {
+    if (dolhins < koalas) {
+        return `Koalas win (${koalas} vs. ${dolhins})`
+    } else if (dolhins > koalas) {
+        return `Dolhins win (${dolhins} vs. ${koalas})`
+    } else {
+        return `Koalas And Dolhins wins (0${dolhins})`
+    }
+}
+
+console.log(checkWinner(avgDolhins, avgKoalas));
+
+
+
 //LECTURE: Introduction to Arrays
 
 const populations = new Array(6.9, 67.4, 56, 1150);
